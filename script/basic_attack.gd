eb78832d4bc86585fbac9a5af8998496fc4131de
nemepop <1070942910@qq.com>
2024-05-28 14:07:29 +0800

    加入体术动画，方便看

--- script/basic_attack.gd
+++ script/basic_attack.gd
@@ -4,7 +4,7 @@ extends Area2D
 @onready var kickarea = $kickarea
 
 const basic_colddown = 1
-const kick_range = 10
+const kick_range = 50
 const kick_basic_damage = 10
 
 var shoot_ready = true
@@ -12,7 +12,6 @@ var kick_ready = true
 func _ready():
 	shoot_timer.wait_time = basic_colddown * (1 - player_var.colddown_reduce)
 	kick_timer.wait_time = basic_colddown * (1 - player_var.colddown_reduce)
-	
 	$kickarea/kickarea_box.set_scale(Vector2(player_var.range_add_ratio,player_var.range_add_ratio))
 func _on_shoot_timer_timeout():
 	shoot_ready = true
@@ -23,11 +22,13 @@ func _on_kick_timer_timeout():
 func enemy_near(a,b):
 	return global_position.distance_to(a.global_position) < global_position.distance_to(b.global_position)
 func _physics_process(delta):
+	
+	$kickarea/kick_anime.play("none")
 	var enemy_in_range = get_overlapping_bodies() #area范围内的物体
 	if !enemy_in_range.is_empty():
-		#enemy_in_range.sort_custom(sort_enemy)
+		
 		var target_enemy =enemy_in_range.reduce(func(min,val):return val if enemy_near(val,min) else min)
-		#var target_enemy = enemy_in_range.back()
+		
 		look_at(target_enemy.global_position)
 		if shoot_ready:
 			shoot()
@@ -36,6 +37,7 @@ func _physics_process(delta):
 			
 		if kick_ready and global_position.distance_to(target_enemy.global_position) < kick_range * player_var.range_add_ratio:
 			kick()
+			print("kick")
 			kick_ready = false
 			kick_timer.start()
 	pass
@@ -50,10 +52,12 @@ func shoot():
 func kick():
 	var enemy_in_kick_range = kickarea.get_overlapping_bodies()
 	#todo:体术动画
+	$kickarea/kick_anime.play("kick")
 	for enemy in enemy_in_kick_range:
 		if enemy.has_method("take_damage"):
 			enemy.take_damage(player_var.player_make_damage(kick_basic_damage))
 	print("kick")
+	
 
 
 
