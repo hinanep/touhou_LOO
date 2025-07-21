4ff1d13be3d9d1069c5f107d276f72c5d0758580
BTO <787874550@qq.com>
2025-07-22 01:44:05 +0800

    优化自机近战攻击特效；重做记忆碎片和飞碟图标，并为它们添加了炫彩效果

--- bto_temp/rotate.gd
+++ bto_temp/rotate.gd
@@ -1,4 +1,9 @@
+#该脚本实现一些贴图的旋转
+
 extends GPUParticles2D
 
+@export_group("Auto Rotate")
+@export var rotate_speed : int
+
 func _process(delta: float) -> void:
-	rotate(4 * PI * delta)
+	rotate(rotate_speed * PI * delta)
