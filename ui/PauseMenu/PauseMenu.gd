c29e3ec0528f53a4e5d320d50e612c4e5a509270
KN_MIZUKI\10709 <1070942910@qq.com>
2026-05-23 03:36:03 +0800

    暂停界面显示属性

--- ui/PauseMenu/PauseMenu.gd
+++ ui/PauseMenu/PauseMenu.gd
@@ -1,16 +1,32 @@
 extends BaseGUIView
 
+@onready var _properties_root: CanvasLayer = $pp
 
 func _on_back_button_pressed():
 
 	G.get_gui_view_manager().clear_to_start()
 
 var is_pausing = false
+
+func _is_level_up_open() -> bool:
+	var vm = G.get_gui_view_manager()
+	for view in vm.viewInstanceMap.values():
+		if view.config.id == &"LevelUp":
+			return true
+	return false
+
+func _sync_properties_visibility() -> void:
+	_properties_root.visible = not _is_level_up_open()
+
+func _process(_delta: float) -> void:
+	_sync_properties_visibility()
+
 func _open():
 	if get_tree().paused:
 		is_pausing = true
 	else:
 		get_tree().paused = true
+	_sync_properties_visibility()
 
 func _unhandled_key_input(event: InputEvent) -> void:
 
