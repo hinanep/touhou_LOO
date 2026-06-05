7f9c11daafaae2c60e3ee6d4678128aa53fad87c
BTO <787874550@qq.com>
2026-06-05 18:54:54 +0800

    新增了四色飞碟各自的发光图层图像资源；新增ufo_enemy_glow脚本，用于控制飞碟发光图层的动画；修改了ufo_enemy_anime脚本，添加了注释，新增了飞碟大小周期性变化的动画。目前存在的问题：不清楚飞碟动画是如何开始的，暂时将飞碟发光图层动画设置为自动播放，可能会和飞碟动画错开；不清楚飞碟颜色是如何控制的，目前只做了蓝色的飞碟外发光，需要将其它几个颜色的补充上；不清楚彩碟是如何运作的，飞碟动画和外发光都没有为彩碟做出适配，大概率无法正常在彩碟上运行。

--- scene/enemys/ufo/ufo_enemy_anime.gd
+++ scene/enemys/ufo/ufo_enemy_anime.gd
@@ -8,22 +8,33 @@ extends AnimatedSprite2D
 @export var b:int;
 ## 最大倾角，单位为角度
 @export var theta:float;
+## 缩放振幅
+@export var amp_scale:float;
 
-var x:float = 0;
+## 自变量
+var x:float;
+## 自变量变化量
 var deltax:float;
+## 最大倾角，单位为弧度
 var theta_rad:float;
+## 缩放平均值
+var avg_scale:float;
 
 func _ready() -> void:
+	## 变量初始化
+	x = 0;
 	deltax = 2 * PI / cycle;
 	theta_rad = (2 * PI) * theta / 360;
+	avg_scale = scale.x;
 
 func _process(delta: float) -> void:
+	## 令贴图沿椭圆轨道周期性运动
 	position = Vector2(a * cos(x), b * sin(x));
+	## 令贴图周期性左右摆动
 	rotation = (-1 * theta_rad * cos(x));
-	print("cycle=")
-	print(cycle)
-	print("2 * PI / cycle=")
-	print(2 * PI / cycle)
+	## 令贴图大小周期性变化
+	scale = Vector2(avg_scale + amp_scale * sin(x), avg_scale + amp_scale * sin(x));
+	## 每帧更新自变量
 	x += (deltax * delta);
 	if x >= 2 * PI:
 		x -= 2 * PI;
